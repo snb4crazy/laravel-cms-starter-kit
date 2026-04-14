ee693d50850d0acd2ba44e0e47cb4755065a9dbb
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-04-13 22:38:21 -0400

    Routes: inertia & filament routes integration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+$defaultPanel = config('admin.default_panel', 'filament');
+
+if ($defaultPanel === 'filament' && config('admin.panels.filament.enabled', true)) {
+    $filamentRoutes = base_path('routes/admin/filament.php');
+
+    if (file_exists($filamentRoutes)) {
+        require $filamentRoutes;
+    }
+}
+
+if ($defaultPanel === 'inertia' && config('admin.panels.inertia.enabled', true)) {
+    $inertiaRoutes = base_path('routes/admin/inertia.php');
+
+    if (file_exists($inertiaRoutes)) {
+        require $inertiaRoutes;
+    }
+}
+
