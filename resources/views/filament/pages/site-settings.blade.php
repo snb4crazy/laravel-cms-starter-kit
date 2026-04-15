39e73ca212a6e8dab3d847fcf54da1c3d108b767
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-04-14 21:09:32 -0400

    Filament: HotFix site-settings blade template

--- resources/views/filament/pages/site-settings.blade.php
+++ resources/views/filament/pages/site-settings.blade.php
@@ -2,14 +2,14 @@
     {{--
         Stub Blade view for the SiteSettings custom page.
 
-        x-filament-panels::page  renders the standard Filament page chrome
+        x-filament-panels::page renders the standard Filament page chrome
         (breadcrumbs, header, notifications area).
 
-        x-filament::form         renders the form defined in SiteSettings::form().
-        wire:submit="save"       calls the save() Livewire action on submit.
+        For custom pages in this template, render the schema with {{ $this->form }}
+        and use a native <form> wrapper for Livewire submit handling.
     --}}
 
-    <x-filament::form wire:submit="save">
+    <form wire:submit="save">
 
         {{ $this->form }}
 
@@ -19,6 +19,6 @@
             </x-filament::button>
         </div>
 
-    </x-filament::form>
+    </form>
 </x-filament-panels::page>
 
