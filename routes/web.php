68578abd7dccc1c10c451e5f5bfeb4a007536193
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-04-14 21:10:26 -0400

    Filament: HotFix Enable FrontMaintenanceMode

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Middleware\FrontMaintenanceMode;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(FrontMaintenanceMode::class)->group(function (): void {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
 
 $defaultPanel = config('admin.default_panel', 'filament');
