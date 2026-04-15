1c40d9a376378ff00673225df8e0c47a2ba16eb2
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-04-15 00:07:19 -0400

    Filament: DB: fix migration - add down() method & uuid

--- database/migrations/2026_04_14_040228_create_media_table.php
+++ database/migrations/2026_04_14_040228_create_media_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->id();
 
             $table->morphs('model');
-            $table->uuid()->nullable()->unique();
+            $table->uuid('uuid')->nullable()->unique();
             $table->string('collection_name');
             $table->string('name');
             $table->string('file_name');
@@ -28,5 +28,10 @@ return new class extends Migration
 
             $table->nullableTimestamps();
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
     }
 };
